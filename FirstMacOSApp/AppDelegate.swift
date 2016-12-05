67252880d30a039e5f26bbfa4b71f90543eb747d
Vijayasundar C <vijayasundar91@gmail.com>
2016-12-04 22:17:39 -0600

    Initial Commit

---
+++ FirstMacOSApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  FirstMacOSApp
+//
+//  Created by Vijayasundar Chinnadurai on 12/4/16.
+//  Copyright © 2016 Vijayasundar Chinnadurai. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ FirstMacOSApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  FirstMacOSApp
+//
+//  Created by Vijayasundar Chinnadurai on 12/4/16.
+//  Copyright © 2016 Vijayasundar Chinnadurai. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
